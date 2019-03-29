6940f4df983d4ff0e5562c00ec6dd203df2e8ef8
Luiz G. Galvão <llgalvao24@gmail.com>
2019-03-29 20:56:06 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,11 +32,15 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TEST 4: seller seller insert====");
+		System.out.println("=== TEST 4: seller insert====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("New id = " + newSeller.getId());
 		
-		
+		System.out.println("=== TEST 5: seller update====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Upadate Completed");
 	}
 }
