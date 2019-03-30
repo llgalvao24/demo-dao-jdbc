a809acd3fab7d8fbdef682c81aba9e6cff2ddc8a
Luiz G. Galvão <llgalvao24@gmail.com>
2019-03-29 22:03:28 -0300

    dep update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -13,6 +13,17 @@ public class Program2 {
 		System.out.println("=== TEST 1: findById =======");
 		Department dep = departmentDao.findById(1);
 		System.out.println(dep);
+		
+		System.out.println("\n=== TEST 2: insert =======");
+		Department newDepartment = new Department(null, "Music");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id: " + newDepartment.getId());
+
+		System.out.println("\n=== TEST 3: update =======");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
 
 	}
 
