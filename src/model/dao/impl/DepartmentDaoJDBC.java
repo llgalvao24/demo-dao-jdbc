65e529899ecf03991bc8dfc43a1ad14882c9a5d0
Luiz G. Galvão <llgalvao24@gmail.com>
2019-03-29 21:59:51 -0300

    dep insert implementation

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -27,13 +27,12 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 		try {
 			st = conn.prepareStatement(
 					"INSERT INTO department "
-					+ "(Id, Name) "
+					+ "(Name) "
 					+ "VALUES "
-					+ "(?, ?)",
+					+ "(?)",
 					Statement.RETURN_GENERATED_KEYS);
 			
-			st.setInt(1, obj.getId());
-			st.setString(2, obj.getName());
+			st.setString(1, obj.getName());
 			
 			int rowsAff = st.executeUpdate();
 			
